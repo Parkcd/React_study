83a46de20863378369f69068904aa6e438008e9e
ParkSungHo <moonhj0105@naver.com>
2022-08-10 19:54:09 +0900

    0810_update

--- splitting-sample/src/App.js
+++ splitting-sample/src/App.js
@@ -1,29 +1,31 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import loadable from '@loadable/component';
+const SplitMe = loadable(() => import('./SplitMe'), {
+    fallback: <div>loading...</div>
+});
+// loadable을 사용하는 부분을 수정하여 로딩 중에 다른 UI를 보여줌
 
-class App extends Component {
-  state = {
-    SplitMe: null,
+function App() {
+  const [visible, setVisible] = useState(false);
+  const onClick = () => {
+    setVisible(true);
   };
-  handleClick = async () => {
-    const loadedModule = await import("./SplitMe");
-    this.setState({
-      SplitMe: loadedModule.default,
-    });
+  const onMouseOver = () => {
+    SplitMe.preload();
   };
-  render() {
-    const { SplitMe } = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={this.handleClick}>Hello React!</p>
-          {SplitMe && <SplitMe />}
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+           Hello React!
+        </p>
+        {visible && <SplitMe />}
+      </header>
+    </div>
+  );
 }
 
 export default App;
